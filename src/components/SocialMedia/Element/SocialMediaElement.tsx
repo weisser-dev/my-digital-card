442d380fa6a6bd5f8abe5752d121e6e855235f10
Erik <erik.weisser@gmail.com>
2024-01-23 14:02:01 +0100

    small adjustments and whatsapp support

--- src/components/SocialMedia/Element/SocialMediaElement.tsx
+++ src/components/SocialMedia/Element/SocialMediaElement.tsx
@@ -16,6 +16,8 @@ const SocialMediaElement: React.FC<SocialMediaElementProps> = ({href}) => {
       'youtube.com': {platform: 'YouTube', icon: 'fab fa-youtube'},
       'pinterest.com': {platform: 'Pinterest', icon: 'fab fa-pinterest'},
       'snapchat.com': {platform: 'SnapChat', icon: 'fab fa-snapchat'},
+      'github.com': {platform: 'GitHub', icon: 'fab fa-github'},
+      'wa.me': {platform: 'WhatsApp', icon: 'fab fa-whatsapp'},
     };
 
     for (const [platformURL, info] of Object.entries(platformMappings)) {
@@ -31,14 +33,14 @@ const SocialMediaElement: React.FC<SocialMediaElementProps> = ({href}) => {
   const title = `Visit ${platform}`;
 
   return (<a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label={title}
-      className={`${styles.socialMediaLink} ${platform.toLowerCase()}`}
-    >
-      <i className={`${styles.socialMediaIcon} ${platformIcon}`}></i>
-    </a>);
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label={title}
+    className={`${styles.socialMediaLink} ${platform.toLowerCase()}`}
+  >
+    <i className={`${styles.socialMediaIcon} ${platformIcon}`}></i>
+  </a>);
 };
 
 export default SocialMediaElement;
