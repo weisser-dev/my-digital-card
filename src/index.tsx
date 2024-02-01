141731bdbecf9a5182104d49ffa655b14995c8ef
Erik <erik.weisser@gmail.com>
2024-02-01 19:05:59 +0100

    remove .env and public url

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import config from './config/config.json';
 import siteData from './data/siteData.json';
 import './index.css';
@@ -55,5 +56,7 @@ loadTheme();
 setMetaTags();
 
 ReactDOM.render(<React.StrictMode>
-  <App/>
+  <Router>
+      <App/>
+  </Router>
 </React.StrictMode>, document.getElementById('root'));
